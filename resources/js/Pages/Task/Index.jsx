2177cc93e6739160d45a962750df850699103932
iamggragas <iamggragas@gmail.com>
2024-06-15 15:33:01 -0400

    Render Tasks

---
+++ resources/js/Pages/Task/Index.jsx
@@ -0,0 +1,185 @@
+import Pagination from '@/Components/Pagination';
+import SelectInput from '@/Components/SelectInput';
+import TextInput from '@/Components/TextInput';
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from '@/constants';
+import Authenticated from '@/Layouts/AuthenticatedLayout'
+import { Head, Link, router } from '@inertiajs/react';
+import React from 'react'
+import TableHeading from '@/Components/TableHeading';
+
+const Index = ({auth, tasks, queryParams = null}) => {
+
+    queryParams = queryParams || {};
+
+    const searchFieldChanged = (name, value) => {
+        if (value) {
+            queryParams[name] = value;
+        }  else {
+            delete queryParams[name];
+        }
+
+        router.get(route('task.index'), queryParams)
+    }
+
+    const onKeyPress = (name, e) => {
+        if (e.key !== 'Enter') return
+
+        searchFieldChanged(name, e.target.value)
+    }
+
+    const sortChanged = (name) => {
+        if (name === queryParams.sort_field) {
+            if (queryParams.sort_direction === 'asc') {
+                queryParams.sort_direction = 'desc';
+            } else {
+                queryParams.sort_direction = 'asc';
+            }
+        } else {
+            queryParams.sort_field = name;
+            queryParams.sort_direction = 'asc';
+        }
+
+        router.get(route('task.index'), queryParams)
+    }
+
+  return (
+    <Authenticated
+        user={auth.user}
+        header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Tasks</h2>}
+    >
+        <Head title="Tasks" />
+
+        <div className="py-12">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="p-6 text-gray-900 dark:text-gray-100">
+                        <div className='overflow-auto'>
+                            {/*<pre>{JSON.stringify(tasks, undefined, 2)}</pre>*/}
+                            <table className='w-full test-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+                            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500'>
+                                <tr className='text-nowrap'>
+                                    <TableHeading 
+                                        name='id' 
+                                        sortable={true} 
+                                        sort_field={queryParams.sort_field} 
+                                        sort_direction={queryParams.sort_direction}
+                                        sortChanged={sortChanged} 
+                                    >
+                                        ID
+                                    </TableHeading>
+                                    <th className='px-3 py-3'>Image</th>
+                                    <TableHeading 
+                                        name='name' 
+                                        sortable={true} 
+                                        sort_field={queryParams.sort_field} 
+                                        sort_direction={queryParams.sort_direction}
+                                        sortChanged={sortChanged} 
+                                    >
+                                        Name
+                                    </TableHeading>
+                                    <TableHeading 
+                                        name='status' 
+                                        sortable={true} 
+                                        sort_field={queryParams.sort_field} 
+                                        sort_direction={queryParams.sort_direction}
+                                        sortChanged={sortChanged} 
+                                    >
+                                        Status
+                                    </TableHeading>
+                                    <TableHeading 
+                                        name='created_at' 
+                                        sortable={true} 
+                                        sort_field={queryParams.sort_field} 
+                                        sort_direction={queryParams.sort_direction}
+                                        sortChanged={sortChanged} 
+                                    >
+                                        Create Date
+                                    </TableHeading>
+                                    <TableHeading 
+                                        name='due_date' 
+                                        sortable={true} 
+                                        sort_field={queryParams.sort_field} 
+                                        sort_direction={queryParams.sort_direction}
+                                        sortChanged={sortChanged} 
+                                    >
+                                        Due Date
+                                    </TableHeading>
+                                    <th className='px-3 py-3'>Created By</th>
+                                    <th className='px-3 py-3 text-right'>actions</th>
+                                </tr>
+                            </thead>
+                            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500'>
+                                <tr className='text-nowrap'>
+                                    <th className='px-3 py-3'></th>
+                                    <th className='px-3 py-3'></th>
+                                    <th className='px-3 py-3'>
+                                        <TextInput 
+                                            className="w-full" 
+                                            defaultValue={queryParams.name}
+                                            placeholder="Task Name" 
+                                            onBlur={e => searchFieldChanged('name', e.target.value)}
+                                            onKey={e => onKeyPress('name', e)}
+                                        />
+                                    </th>
+                                    <th className='px-3 py-3'>
+                                        <SelectInput 
+                                            className="w-full"
+                                            defaultValue={queryParams.status}
+                                            onChange={e => searchFieldChanged('status', e.target.value)}
+                                        >
+                                            <option value="">Select Status</option>
+                                            <option value="pending">Pending</option>
+                                            <option value="in_progress">In Progress</option>
+                                            <option value="completed">Completed</option>
+                                        </SelectInput>
+                                    </th>
+                                    <th className='px-3 py-3'></th>
+                                    <th className='px-3 py-3'></th>
+                                    <th className='px-3 py-3'></th>
+                                    <th className='px-3 py-3'></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {tasks.data.map((task) => (
+                                    <tr className='bg-white border-b dark:gray-800 dark:border-gray-700' key={task.id}>
+                                        <td className='px-3 py-3'>{task.id}</td>
+                                        <td className='px-3 py-3'><img src={task.image_path} alt="" style={{width: 60}} /></td>
+                                        <td className='px-3 py-3'>{task.name}</td>
+                                        <td className='px-3 py-3'>
+                                            <span className={
+                                                "px-2 py-1 rounded text-white " +
+                                                TASK_STATUS_CLASS_MAP[task.status]
+                                            }>
+                                                {TASK_STATUS_TEXT_MAP[task.status]}
+                                            </span>
+                                        </td>
+                                        <td className='px-3 py-3 text-nowrap'>{task.created_at}</td>
+                                        <td className='px-3 py-3 text-nowrap'>{task.due_date}</td>
+                                        <td className='px-3 py-3'>{task.createdBy.name}</td>
+                                        <td className='px-3 py-3'>
+                                            <Link href={route('task.edit', task.id)}
+                                                className='font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1'
+                                            >
+                                                Edit
+                                            </Link>
+                                            <Link href={route('task.destroy', task.id)}
+                                                className='font-medium text-red-600 dark:text-red-500 hover:underline mx-1'
+                                            >
+                                                Delete
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                            </table>
+                        </div>
+                        <Pagination links={tasks.meta.links} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Authenticated>
+  )
+}
+
+export default Index
